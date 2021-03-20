6c5a806641d63c8f76ce7dc23966c7d3815798c3
Fatima <fatima.hammouri@gmail.com>
2021-03-20 10:42:04 +0000

    Modified 8-11 python functions

--- trials.py
+++ trials.py
@@ -62,16 +62,55 @@ def snake_to_camel(string):
 
 
 def longest_word_length(words):
-    pass  # TODO: replace this line with your code
+    longest = len(words[0])
+
+    for word in words:
+        if longest < len(word):
+            longest = len(word)
+    return longest
 
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
+    result = []
+    for char in string:
+        if len(result) == 0 or char != result[len(result) - 1]:
+            result.append(char)
+    return ''.join(result)
 
 
 def has_balanced_parens(string):
-    pass  # TODO: replace this line with your code
+    parens = 0
+
+    for char in string:
+        if char == '(':
+            parens += 1
+        elif char == ')':
+            parens -= 1
+        
+            if parens < 0:
+                return False
+    return parens < 0
+
 
 
 def compress(string):
-    pass  # TODO: replace this line with your code
+    compressed = []
+
+    curr_char = ''
+    char_count = 0
+    for char in string:
+        if char != curr_char:
+            compressed.append(curr_char)
+            if char_count > 1:
+               compressed.append(str(char_count))
+            curr_char = char
+            char_count = 0
+        
+        char_count += 1
+
+    compressed.append(curr_char)
+    if char_count > 1:
+        compressed.append(str(char_count))
+    return ''.join(compressed)
+            
+
