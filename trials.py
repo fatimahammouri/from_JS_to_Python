3bb7a038b36c0ffb6874680dbf2fe4356884111d
Fatima <fatima.hammouri@gmail.com>
2021-03-20 06:59:59 +0000

    Modified 6th python function

--- trials.py
+++ trials.py
@@ -42,7 +42,14 @@ def get_range(start, stop):
     return nums
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    chars = []
+
+    for letter in word:
+        if letter in 'aeiou':
+            chars.append('*')
+        else:
+            chars.append(letter)
+    return ''.join(chars)
 
 
 def snake_to_camel(string):
